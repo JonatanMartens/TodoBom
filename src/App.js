cda7e954eef50fa61929ba9fa34345584d7761fd
Jonatan Martens <jonatanmartenstav@gmail.com>
2021-09-19 12:39:21 +0300

    Add header component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Header from "./components/Header";
 import ItemList from "./components/ItemList";
 import TodoList from "./todolist/TodoList";
 
@@ -6,7 +7,13 @@ function App() {
   const todoList = new TodoList();
   todoList.create("test");
   todoList.create("aa");
-  return <ItemList todoList={todoList} />;
+  return (
+    <div>
+      <Header></Header>
+
+      <ItemList todoList={todoList} />
+    </div>
+  );
 }
 
 export default App;
