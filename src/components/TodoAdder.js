2d8de530f5d939ff61b51fcec455c90fff026702
Jonatan Martens <jonatanmartenstav@gmail.com>
2021-09-19 15:25:54 +0300

    Show error when adding

--- src/components/TodoAdder.js
+++ src/components/TodoAdder.js
@@ -4,25 +4,36 @@ import { useState } from "react";
 export default function TodoAdder(props) {
   const { todoList } = props;
   const [todoText, setTodoText] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleChange = (event) => {
     setTodoText(event.target.value);
   };
 
   const handleSubmit = (event) => {
-    todoList.create(todoText);
-    setTodoText("");
+    try {
+      todoList.create(todoText);
+      setTodoText("");
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
     event.preventDefault();
   };
 
   return (
-    <form className="todoAdder" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Enter a new todo and press enter"
-        onChange={handleChange}
-        value={todoText}
-      ></input>
-    </form>
+    <div className="todoAdder">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter a new todo and press enter"
+          onChange={handleChange}
+          value={todoText}
+        ></input>
+      </form>
+
+      <p hidden={errorMessage ? false : true} className="errorMessage">
+        {errorMessage}
+      </p>
+    </div>
   );
 }
